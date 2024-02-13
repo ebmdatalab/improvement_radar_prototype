e2cb7d350752ca5f232dce6be2a58058b070f95f
Richard Croker <ra.croker@gmail.com>
2024-02-13 09:41:19 +0000

    update y-axis and re-run

--- notebooks/diffable_python/SICBL_change_detection_report_no_cd.py
+++ notebooks/diffable_python/SICBL_change_detection_report_no_cd.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 # ---
 # jupyter:
 #   jupytext:
@@ -358,13 +359,13 @@ ccg_names = bq.cached_read(sql, os.path.join(DATA_FOLDER, "ccg_names.csv"), use_
 # This tool identifies sub-ICB locations (SICBLs) which have shown substantial improvement across each of our OpenPrescribing measures. The five SICBLs with the largest improvement are reported. We hope this will stimulate discussion with areas that have made effective changes so that successful strategies can be shared.
 #
 # ## How it works
-# We used trend indicator saturation to detect the timing and size of changes of SICBLs against all SICBLs in England. This prototype uses the current criteria to identify improvement:
-# * SICBLs needed to, on average, be in the highest 20% during the first 6 months of the time period shown.
+# The tool currently uses the following criteria to identify improvement:
+# * SICBLs needed to be, on average, in the highest 20% during the first 6 months of the time period shown.
 # * SICBLs needed to improve to be, on average, in the lowest 50% of SICBLs during the last 6 months of the period shown.
-# * The rate of the measure has to decrease by at least 5% across the time period shown (calculated as the difference between the average rate for the first 6 months and the last 6 months). 
+# * The rate of the measure has to decrease by at least 5% across the time period shown (calculated as the difference between the average rate for the first 6 months and the last 6 months).
 # * There needed to be, on average, at least 50 prescription items written.
 #
-# You can find more information on the trend indicator saturation methodology we use [here](https://www.bmj.com/content/367/bmj.l5205), including a podcast on our work with Professor Ben Goldacre.
+# We are continuing to review how we might further optimise these criteria to detect even more interesting changes in future. For example, we may be able to introduce elements of a ‘trend indicator saturation’ methodology (which we have used in previous research). You can find more information on this [here](https://www.bmj.com/content/367/bmj.l5205), including a podcast on our work with Professor Ben Goldacre.
 #
 # ## Interpretation notes
 # These pilot results are provided for the interest of advanced users, although we don't know how relevant they are in practice. There is substantial variation in prescribing behaviours, across various different areas of medicine. Some variation can be explained by demographic changes, or local policies or guidelines, but much of the remaining variation is less easy to explain.
@@ -419,6 +420,6 @@ for m in measure_list['measure_name']:
              
         else:
             display(Markdown("No organisations met the technical criteria for detecting substantial change on this measure."))
-# +
+# -
 
 
