5006de62be9a21eb6ce54ba39356d3d66e1f8052
David Evans <dave.evans@thedatalab.org>
2024-04-22 18:32:19 +0100

    Install and configure requirements

--- postinstall.sh
+++ postinstall.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
 # This script is run after installing apt and Python packages
+
+# This import has the side-effect of installing the required R pacakges the
+# first time it's run
+python -c 'import change_detection.functions'
